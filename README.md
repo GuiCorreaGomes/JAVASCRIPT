c3ff704a2edbeeea62007aadc10fe79d3e81ef12
GuiCorreaGomes <guicorreagomes@gmail.com>
2023-05-15 12:00:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JAVASCRIPT
+ Cursoe de Javascript
---
+++ aula.js
@@ -0,0 +1,15 @@
+let nome = 'Guilherme';
+let idade = 25;
+let estaAprovado = true;
+let sobrenome = undefined;
+let corSelecionado = null;
+
+let pessoa = { 
+    nome: 'Guilherme',
+    idade: 29,
+    estaAprovado: true,
+    sobrenome: 'Correa'
+
+}
+
+console.log(pessoa);
